520813df33049e77d4703515956ceaa7651cf47c
Hambuger <hambuger011@gmail.com>
2020-12-08 18:28:15 +0800

    no message

--- sus_scriptsTruck1.0.14/Assets/Scripts/CSharp/SDK/Firebase/FirebaseSdkMrg.cs
+++ sus_scriptsTruck1.0.14/Assets/Scripts/CSharp/SDK/Firebase/FirebaseSdkMrg.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Firebase;
 using Firebase.DynamicLinks;
@@ -39,54 +40,34 @@ public class FirebaseSdkMrg : MonoBehaviour
         });
     }
 
-    private void FBShareLinkSucced(string postId)
+    private void OnGUI()
     {
-        TalkingDataManager.Instance.ShareRecord(2);
-        LOG.Info("--ShareSucc--->postId:" + postId);
-
-        var Localization = GameDataMgr.Instance.table.GetLocalizationById(142);
-        UITipsMgr.Instance.PopupTips(Localization, false);
+        if (GUI.Button(new Rect(400, 100f, 100f, 100f), "测试1"))
+        {
+            Uri uri = CreateAndDisplayLongLink();
+            // Uri uri = CreateAndDisplayLongLink2();
+            // SdkMgr.Instance.shareSDK.ShareMsg(uri.ToString());
+            LOG.Log(uri.ToString());
+        }
+    
+        if (GUI.Button(new Rect(400, 200f, 100f, 100f), "测试2"))
+        {
+            var uu = CreateAndDisplayShortLinkAsync();
+            LOG.Log(uu.ToString());
+            // if (null != uri &&!string.IsNullOrEmpty(uri.ToString())) 
+            //   Application.OpenURL(uri.ToString());
+    
+            // SdkMgr.Instance.shareSDK.ShareScreenShot();
+        }
+    
+        if (GUI.Button(new Rect(400, 300f, 100f, 100f), "测试3"))
+        {
+            var oo = CreateAndDisplayUnguessableShortLinkAsync();
+            LOG.Log(oo.ToString());
+    
+            // SdkMgr.Instance.shareSDK.CopyToClipboard("==========CopyToClipboard========");
+        }
     }
-
-    private void FBShareLinkFaild(bool isCancel, string errorInfo)
-    {
-        var Localization = GameDataMgr.Instance.table.GetLocalizationById(143);
-        UITipsMgr.Instance.PopupTips(Localization, false);
-
-        //UITipsMgr.Instance.PopupTips("Share Failed!", false);
-    }
-
-    private DynamicLinkComponents components;
-    private Uri uri;
-
-    // private void OnGUI()
-    // {
-    //     if (GUI.Button(new Rect(400, 100f, 100f, 100f), "测试1"))
-    //     {
-    //         uri = CreateAndDisplayLongLink();
-    //         SdkMgr.Instance.shareSDK.ShareMsg(uri.ToString());
-    //     }
-    //
-    //     if (GUI.Button(new Rect(400, 200f, 100f, 100f), "测试2"))
-    //     {
-    //         CreateAndDisplayShortLinkAsync();
-    //
-    //         // if (null != uri &&!string.IsNullOrEmpty(uri.ToString())) 
-    //         //   Application.OpenURL(uri.ToString());
-    //
-    //         // SdkMgr.Instance.shareSDK.ShareScreenShot();
-    //     }
-    //
-    //     if (GUI.Button(new Rect(400, 300f, 100f, 100f), "测试3"))
-    //     {
-    //         CreateAndDisplayUnguessableShortLinkAsync();
-    //
-    //         // if (null != uri &&!string.IsNullOrEmpty(uri.ToString()))
-    //         //   SdkMgr.Instance.facebook.FBShareLink(uri.ToString(), "Secrets of game choices", "Welcome to Secrets", "", FBShareLinkSucced, FBShareLinkFaild);
-    //
-    //         // SdkMgr.Instance.shareSDK.CopyToClipboard("==========CopyToClipboard========");
-    //     }
-    // }
 
     // Update is called once per frame
     void Update()
@@ -105,14 +86,95 @@ public class FirebaseSdkMrg : MonoBehaviour
         Debug.LogError("Firebase初始化完成===============================");
         firebaseInitialized = true;
     }
+    
+    /// <summary>
+    /// 获取一个类指定的属性值
+    /// </summary>
+    /// <param name="info">object对象</param>
+    /// <param name="field">属性名称</param>
+    /// <returns></returns>
+    public static object GetPropertyValue(object info, string field)
+    {
+        if (info == null) return null;
+        Type t = info.GetType();
+        IEnumerable<System.Reflection.PropertyInfo> property = from pi in t.GetProperties() where pi.Name.ToLower() == field.ToLower() select pi;
+        return property.First().GetValue(info, null);
+    }
 
     // Display the dynamic link received by the application.
     void OnDynamicLink(object sender, EventArgs args)
     {
-        Debug.LogError("OnDynamicLink===============================");
+        Debug.LogWarning("OnDynamicLink===============================");
         var dynamicLinkEventArgs = args as ReceivedDynamicLinkEventArgs;
-        Debug.LogErrorFormat("Received dynamic link {0}",
-            dynamicLinkEventArgs.ReceivedDynamicLink.Url.OriginalString);
+        Debug.LogWarningFormat("Received dynamic link {0}", dynamicLinkEventArgs.ReceivedDynamicLink.Url.OriginalString);
+        Debug.LogWarningFormat("sender {0}", sender);
+        var refferer = GetPropertyValue(sender, "refferer");
+        Debug.LogWarningFormat("refferer {0}", refferer);
+        
+        //数组 转 object
+        object[] objectArray = new object[2];//这里的2就是改成你要传递几个参数
+        objectArray[0] = true;
+        objectArray[1] = 5f;
+        object objArr = (object)objectArray;
+        Debug.LogWarningFormat("objArr {0}", objArr);
+        // object[] objArr = (object[])sender;
+        // Debug.LogWarningFormat("objArr.Length {0}", objArr.Length);
+        // for (int i = 0; i < objArr.Length; i++)
+        // {
+        //     Debug.LogWarningFormat("objArr {0}==={1}",i, objArr[i]);
+        // }
+    }
+
+    public Uri CreateAndDisplayLongLink2()
+    {
+#if UNITY_5_6_OR_NEWER
+        string appIdentifier = Application.identifier;
+#else
+      string appIdentifier = Application.bundleIdentifier;
+#endif
+        var components = new Firebase.DynamicLinks.DynamicLinkComponents(
+            // The base Link.
+            new System.Uri("https://www.baidu.com/?TestId=" + 88888888),
+            // The dynamic link URI prefix.
+            kDomainUriPrefix) {
+            IOSParameters = new Firebase.DynamicLinks.IOSParameters(appIdentifier),
+            AndroidParameters = new Firebase.DynamicLinks.AndroidParameters(
+                appIdentifier),
+        };
+// do something with: components.LongDynamicLink
+
+        var options = new Firebase.DynamicLinks.DynamicLinkOptions {
+            PathLength = DynamicLinkPathLength.Unguessable
+        };
+        
+        Firebase.DynamicLinks.DynamicLinks.GetShortLinkAsync(components, options).ContinueWith(task => {
+            if (task.IsCanceled) {
+                Debug.LogError("GetShortLinkAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("GetShortLinkAsync encountered an error: " + task.Exception);
+                return;
+            }
+        
+            // Short Link has been created.
+            Firebase.DynamicLinks.ShortDynamicLink link = task.Result;
+            Debug.LogFormat("Generated short link {0}", link.Url);
+        
+            var warnings = new System.Collections.Generic.List<string>(link.Warnings);
+            if (warnings.Count > 0) {
+                // Debug logging for warnings generating the short link.
+            }
+        });
+
+        return components.LongDynamicLink;
+    }
+
+    public Uri CreateAndDisplayLongLink()
+    {
+        var longLink = CreateDynamicLinkComponents().LongDynamicLink;
+        Debug.Log(String.Format("Long dynamic link {0}", longLink));
+        return longLink;
     }
 
     DynamicLinkComponents CreateDynamicLinkComponents()
@@ -164,13 +226,6 @@ public class FirebaseSdkMrg : MonoBehaviour
                 ImageUrl = new System.Uri("https://mysite.com/someimage.jpg")
             },
         };
-    }
-
-    public Uri CreateAndDisplayLongLink()
-    {
-        var longLink = CreateDynamicLinkComponents().LongDynamicLink;
-        Debug.Log(String.Format("Long dynamic link {0}", longLink));
-        return longLink;
     }
 
     public Task<ShortDynamicLink> CreateAndDisplayShortLinkAsync()
