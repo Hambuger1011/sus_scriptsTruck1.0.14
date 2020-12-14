4fc81882c03ca476559e50dd1c43ab36a783d53f
Hambuger <hambuger011@gmail.com>
2020-12-14 11:10:40 +0800

    no message

--- sus_scriptsTruck1.0.14/Assets/Scripts/Lua/Logic/UI/UI_FirstCharge/UIFirstChargeForm.lua
+++ sus_scriptsTruck1.0.14/Assets/Scripts/Lua/Logic/UI/UI_FirstCharge/UIFirstChargeForm.lua
@@ -6,6 +6,12 @@ local base = UIView
 
 local uiid = logic.uiid
 
+local PosList = {
+    core.Vector2.New(232,150),core.Vector2.New(83,280),
+    core.Vector2.New(-220,150),core.Vector2.New(-83,280),
+}
+local RewardTrans = {}
+
 UIFirstChargeForm.config = {
     ID = uiid.UIFirstChargeForm,
     AssetName = 'UI/Resident/UI/UIFirstChargeForm'
@@ -17,10 +23,20 @@ function UIFirstChargeForm:OnInitView()
     UIView.OnInitView(self)
     local get = logic.cs.LuaHelper.GetComponent
     local root = self.uiform.transform
+    RewardTrans = {}
 
+    self.Reward =CS.DisplayUtil.GetChild(root.gameObject, "Reward"):GetComponent(typeof(logic.cs.CanvasGroup))
+    self.TreasureBox =CS.DisplayUtil.GetChild(root.gameObject, "TreasureBox")
+    self.Keys =CS.DisplayUtil.GetChild(root.gameObject, "Keys")
+    self.Diamonds =CS.DisplayUtil.GetChild(root.gameObject, "Diamonds")
+    self.ClothesCoupon =CS.DisplayUtil.GetChild(root.gameObject, "ClothesCoupon")
+    self.OptionCoupon =CS.DisplayUtil.GetChild(root.gameObject, "OptionCoupon")
+    RewardTrans = {self.Keys,self.Diamonds,self.ClothesCoupon,self.OptionCoupon}
+    
     self.GoBtn = get(root,'Bg/GoBtn',typeof(logic.cs.Button))
     self.Close = get(root,'Bg/Close',typeof(logic.cs.Button))
     self.Tile = get(root,'Bg/Tile',typeof(logic.cs.Text))
+    
     self.GoBtn.onClick:RemoveAllListeners()
     self.GoBtn.onClick:AddListener(function()
         self:OnGoBtn()
@@ -31,8 +47,7 @@ function UIFirstChargeForm:OnInitView()
         self:OnExitClick()
     end)
 
-    --刷新展示
-    --self:UpdateShow(get,root);
+    self:PlayAnim();
 end
 
 function UIFirstChargeForm:OnOpen()
@@ -46,45 +61,17 @@ function UIFirstChargeForm:OnClose()
 
 end
 
---刷新展示
-function UIFirstChargeForm:UpdateShow(get,root)
+function UIFirstChargeForm:PlayAnim()
+    local duration = 1
+    self.Reward.alpha = 0
+    self.Reward:DOFade(1,duration)
 
-    self.Tile.text=CS.CTextManager.Instance:GetText(343);  -- "自动签到标题"
-    self.SignInBtnText.text=CS.CTextManager.Instance:GetText(344);  -- "签到"
-
-    --id; --奖励id
-    --type; --奖励类型 1钥匙 2钻石 4组合包
-    --bkey_qty; --奖励钥匙
-    --diamond_qty;  --奖励钻石
-    --is_receive; --是否已签到领取 1:是 0否
-
-    if(Cache.SignInCache.activity_login.type==1)then    --1钥匙
-
-        self.Type1:SetActiveEx(true);
-        self.RewardImg.sprite = CS.ResourceManager.Instance:GetUISprite("ADSReward/qd-yaoshi");
-        self.KeyText = get(root,'Canvas/Bg/Type1/KeyImg/KeyText',typeof(logic.cs.Text))
-        self.KeyText.text="X"..Cache.SignInCache.activity_login.bkey_qty;
-
-    elseif(Cache.SignInCache.activity_login.type==2)then  -- 2钻石
-
-        self.Type2:SetActiveEx(true);
-        self.RewardImg.sprite = CS.ResourceManager.Instance:GetUISprite("ADSReward/qd-zuanshi3");
-        self.DiamondText = get(root,'Canvas/Bg/Type2/DiamondImg/DiamondText',typeof(logic.cs.Text))
-        self.DiamondText.text="X"..Cache.SignInCache.activity_login.diamond_qty;
-
-
-    elseif(Cache.SignInCache.activity_login.type==4)then   -- 4组合包
-
-        self.Type3:SetActiveEx(true);
-        self.RewardImg.sprite = CS.ResourceManager.Instance:GetUISprite("ADSReward/qd-zuanshi_yaoshi");
-        self.KeyText = get(root,'Canvas/Bg/Type3/KeyImg/KeyText',typeof(logic.cs.Text))
-        self.DiamondText = get(root,'Canvas/Bg/Type3/DiamondImg/DiamondText',typeof(logic.cs.Text))
-        self.KeyText.text="X"..Cache.SignInCache.activity_login.bkey_qty;
-        self.DiamondText.text="X"..Cache.SignInCache.activity_login.diamond_qty;
-
-
+    for i = 1, #RewardTrans do
+        RewardTrans[i].transform:DOAnchorPos(PosList[i],duration):SetEase(core.tween.Ease.Flash):OnComplete(function()  end)
     end
 
+    self.TreasureBox.transform.localScale = core.Vector3.New(0.5,0.5,1)
+    self.TreasureBox.transform:DOScale(core.Vector3.New(1,1,1),duration):SetEase(core.tween.Ease.OutBack)
 end
 
 
